f4cd8698603296c54706afb4a5131667c32b10fc
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2022-01-04 16:48:52 +0700

    redux app

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import ProductReducer from "../features/ProductSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    product: ProductReducer,
   },
 });
